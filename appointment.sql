4f06cfbc76559e5561813c18a314ecdc2ce02401
syed omer farooq <syedomer.farooq@zemosolabs.com>
2022-01-04 13:49:54 +0530

    added sql file

---
+++ appointment.sql
@@ -0,0 +1,90 @@
+DROP SCHEMA IF EXISTS `appointment_management`;
+
+CREATE SCHEMA `appointment_management`;
+
+use `appointment_management`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(100) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users` 
+VALUES 
+('receptionist@admin.com','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` varchar(100) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('receptionist@admin.com','ROLE_ADMIN');
+
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `patient`;
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `phone_number` varchar(45) DEFAULT NULL, 
+  PRIMARY KEY (`id`),
+  UNIQUE (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `appointment`;
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `doctor_name` varchar(128) DEFAULT NULL,
+  `appointment_date` varchar(128) DEFAULT NULL,
+  `appointment_time` varchar(128) DEFAULT NULL,
+  `reason` varchar(128) DEFAULT NULL,
+  `patient_id` int(11) DEFAULT NULL,
+  
+  PRIMARY KEY (`id`),
+  
+  UNIQUE KEY `DOCTOR_UNIQUE` (`doctor_name`),
+  
+  KEY `FK_PATIENT_ID_idx` (`patient_id`),
+  
+  CONSTRAINT `FK_PATIENT` 
+  FOREIGN KEY (`patient_id`) 
+  REFERENCES `patient` (`id`) 
+  
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
